13f3e454c4e47bb63de36415f58a0359873aec66
karan <karansalunkhe180@gmail.com>
2026-03-19 20:31:48 +0530

    Login show password eye icon

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
-import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import '../styles/Login.css';
 
 const Login = () => {
@@ -87,7 +86,7 @@ const Login = () => {
               }}
 
             >
-              {showPassword ? <FaEyeSlash /> : <FaEye />}
+              {showPassword ? "🙈" : "👁️"}
             </span>
           </div>
         </div>
